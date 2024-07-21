a2bb666065a4eab43f71431e7fafb2f151ab6784
taintpro98 <taideptrai02@gmail.com>
2024-07-22 00:37:46 +0700

    word search

---
+++ leetcode/python/best_time_buy_sell_stock_transaction_fee.py
@@ -0,0 +1,11 @@
+## https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        return 0
+    
+sol = Solution()
+prices = [1,3,2,8,4,9]
+fee = 2
+sol.maxProfit(prices, fee)
--- leetcode/python/word_search.py
+++ leetcode/python/word_search.py
@@ -4,15 +4,53 @@ from typing import List
 
 
 class Solution:
+    def __init__(self) -> None:
+        self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
     def backtrack(
         self,
         board: List[List[str]],
+        visited,
         word,
-        curIdx
+        curIdx,
+        idx,
+        jdx,
+        ans
     ):
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                self.backtrack()
+        if curIdx >= len(word):
+            return
+        if word[curIdx] != board[idx][jdx]:
+            return
+        if curIdx == len(word) - 1:
+            ans[0] = True
+            return
+        for (x, y) in self.directions:
+            ni, nj = x + idx, y + jdx
+            if ni < len(board) and ni >= 0 and nj >=0 and nj < len(board[0]) and (ni, nj) not in visited:
+                visited.add((ni, nj))
+                self.backtrack(board, visited, word, curIdx+1, ni, nj, ans)
+                visited.remove((ni, nj))
 
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.backtrack(board, word, )
+        result = [False]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                visited = set()
+                visited.add((i, j))
+                self.backtrack(board, visited, word, 0, i, j, result)
+                if result[0]:
+                    return True
+        return result[0]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    board = [
+        ["A", "B", "C", "E"], 
+        ["S", "F", "C", "S"], 
+        ["A", "D", "E", "E"]
+    ]
+    word = "ABCCED"
+    result = sol.exist(board, word)
+    print(result)
