b633d0d8c9615e29d9c5f0f961a2040a72c6ca42
taintpro98 <taideptrai02@gmail.com>
2024-10-31 17:07:55 +0700

    lam duoc bai stone game 1 hay phet

--- leetcode/python/stone_game/stone_game.py
+++ leetcode/python/stone_game/stone_game.py
@@ -30,8 +30,56 @@ piles.length is even.
 1 <= piles[i] <= 500
 sum(piles[i]) is odd.
 """
+
+"""
+dp[i][i] = invalid <= because the number has to be odd
+dp[i][i+1] = piles[i]
+dp[i][j] = max()
+- = sum[i][j] - |dp[i][j-1]|
+"""
 from typing import List
 
 class Solution:
+    def handle(self, dp: List[List[int]], piles: List[int], pr: List[int], i: int, j: int) -> int:
+        if dp[i][j] != -1:
+            return dp[i][j]
+        if i == j:
+            dp[i][i] = 0
+            return 0
+        if j - i == 1:
+            dp[i][j] = piles[i]
+            return dp[i][j]
+        sumPiles = pr[j] - pr[i]
+        if self.handle(dp, piles, pr, i, j-1) > 0: # if Bob wins i, j-1
+            a = sumPiles - self.handle(dp, piles, pr, i, j-1) # the number of piles Alice has
+            if a >= self.handle(dp, piles, pr, i, j-1):
+                dp[i][j] = a
+            else:
+                dp[i][j] = -self.handle(dp, piles, pr, i, j-1)
+        else: # if Alice wins i, j-1
+            dp[i][j] = piles[j-1] + abs(self.handle(dp, piles, pr, i, j-1)) # Alice wins i, j
+            
+        if self.handle(dp, piles, pr, i+1, j) > 0: #if Bob wins i+1, j
+            a = sumPiles - self.handle(dp, piles, pr, i+1, j) # the number of piles Alice has
+            if a >= self.handle(dp, piles, pr, i+1, j):
+                dp[i][j] = max(dp[i][j], a)
+            else:
+                dp[i][j] = max(dp[i][j], -self.handle(dp, piles, pr, i+1, j))
+        else: # Alice wins i+1, j
+            dp[i][j] = max(dp[i][j], piles[i] + abs(self.handle(dp, piles, pr, i+1, j)))
+        return dp[i][j]
+            
+        
     def stoneGame(self, piles: List[int]) -> bool:
-        return
+        n = len(piles)
+        pr = [0]
+        for i in range(n):
+            pr.append(piles[i] + pr[-1])
+        dp = [[-1] * (n+1) for _ in range(n)]
+        self.handle(dp, piles, pr, 0, n)
+        return self.handle(dp, piles, pr, 0, n) > 0
+        
+piles = [3,7,2,3]
+sol = Solution()
+ans = sol.stoneGame(piles)
+print(ans)
