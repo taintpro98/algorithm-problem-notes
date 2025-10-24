01c18ac79737fad949907e613a5fcec19c1d528e
taintpro98 <taideptrai02@gmail.com>
2025-10-24 21:17:37 +0700

    max rectangle

--- leetcode/python/stack_queue/max_rectangle.py
+++ leetcode/python/stack_queue/max_rectangle.py
@@ -24,14 +24,40 @@ matrix[i][j] is '0' or '1'.
 """
 from typing import List
 
-
 class Solution:
-    def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        return
+	def largestRectangleArea(self, heights: List[int]) -> int:
+		stack = []
+		ans = 0
+		for (idx, h) in enumerate(heights + [0]):
+			while stack and heights[stack[-1]] >= h:
+				top = stack[-1]
+				stack.pop()
+				l = -1
+				if stack:
+					l = stack[-1]
+				ans = max(ans, heights[top] * (idx - l - 1))
+			stack.append(idx)
+		return ans
 
+	def maximalRectangle(self, matrix: List[List[str]]) -> int:
+		R, C = len(matrix), len(matrix[0])
+		ans = 0
+		for r in range(R):
+			h = []
+			for c in range(C):
+				if matrix[r][c] == "0":
+					h.append(0)
+				else:
+					t = r
+					hs = 0
+					while t < R and matrix[t][c] == "1":
+						hs += 1
+						t += 1
+					h.append(hs)
+			ans = max(ans, self.largestRectangleArea(h))
+		return ans
 
-matrix = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"],
-          ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
+matrix = [["1"]]
 sol = Solution()
 ans = sol.maximalRectangle(matrix)
 print(ans)
